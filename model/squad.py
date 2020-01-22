81230916555a267a4216fc04fbbbdf86147470ff
Joao <bljoviph@gmail.com>
2020-01-22 12:00:01 -0300

    mudancas

--- model/squad.py
+++ model/squad.py
@@ -1,11 +1,36 @@
 class Squad:
-    def __init__(self, id, nome, descricao, numeroPessoas, linguagemBack, frameFront):
+    def __init__(self, id, nome, descricao, numeroPessoas):
         self.id = id
         self.nome = nome
         self.descricao = descricao
         self.numeroPessoas = numeroPessoas
-        self.linguagemBack = linguagemBack
-        self.frameFront = frameFront
+        self.__linguagemBack = None
+        self.__linguagemFront = None
+        self.__sgbd = None
     
     def __str__(self):
         return f'{self.id} {self.nome} {self.descricao} {self.numeroPessoas} {self.linguagemBack} {self.frameFront}' 
+    
+    @property
+    def linguagemBack(self):
+        return self.__linguagemBack
+
+    @linguagemBack.setter
+    def linguagemBack(self, l):
+        self.__linguagemBack = l
+
+    @property
+    def linguagemFront(self):
+        return self.__linguagemFront
+
+    @linguagemFront.setter
+    def linguagemFront(self, l):
+        self.__linguagemFront = l
+
+    @property
+    def sgbd(self):
+        return self.__sgbd
+
+    @sgbd.setter
+    def sgbd(self):
+        self.sgbd
